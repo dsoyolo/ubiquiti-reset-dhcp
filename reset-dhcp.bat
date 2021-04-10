445b4682fa0c48a2fe4671aa85aa82ef6678b1c1
ubimars <marcelpenton+github@gmail.com>
2021-04-10 14:44:40 -0400

    Added %USERPROFILE%

--- reset-dhcp.bat
+++ reset-dhcp.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:/Python39/python.exe" "C:/Users/mpenton/Documents/GitHub/ubiquiti-reset-dhcp/reset-dhcp.py"
+"C:/Python39/python.exe" "%USERPROFILE%/Documents/GitHub/ubiquiti-reset-dhcp/reset-dhcp.py"
 pause
