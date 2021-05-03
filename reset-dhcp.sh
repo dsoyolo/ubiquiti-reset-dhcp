fd82ce1103bc2e9f197fbf32dde90732b13bf1d5
ubimars <marcelpenton+github@gmail.com>
2021-05-03 17:03:27 -0400

    Updated script

--- reset-dhcp.sh
+++ reset-dhcp.sh
@@ -4,16 +4,14 @@ run=/opt/vyatta/bin/vyatta-op-cmd-wrapper
 # Set port to reset
 PORT='eth8'
 
-IP=$(/sbin/ifconfig $PORT | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+IP=$(/sbin/ifconfig $PORT | grep 'inet' | cut -d: -f2 | awk '{print $2}')
 NETWORK=${IP%%.*}
 
 if [ "$NETWORK" = 192 ]
 then
-  echo 'resetting DHCP ...'
-  $run renew dhcp interface eth0
-  echo 'done'
-  exit 0
+  echo Resetting DHCP...
+  $run renew dhcp interface $PORT
+  echo Done.
 else
-  echo 'reset DHCP not needed ... exiting'
-  exit 1
+  echo DHCP reset not needed.
 fi
